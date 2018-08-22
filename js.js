973e6bb09400013bc35d85d2ac4b6218e503e4bd
bdsummer2018 <example@i.ua>
2018-08-22 19:04:55 +0300

    fdgsdfhg

---
+++ js.js
@@ -0,0 +1,47 @@
+//Example 1
+// var aGrad = 180;
+// var aRad = aGrad*Math.PI/180;
+// var a = Math.cos(aRad);
+// alert(a);
+
+//Teory 1
+// var x = 0;
+// Math.sin(x);
+// Math.cos(x);
+// Math.tan(x);
+// Math.sinh(x);
+// Math.cosh(x);
+// Math.tanh(x);
+// Math.asin(x);
+//
+// Math.pow(x,y);
+// Math.sqrt(x);
+// Math.exp(x);
+//
+// Math.pow(x,1/3);
+//
+//
+// Math.round(x);
+// Math.ceil(x);
+//
+// Math.abs(x);
+//
+// Math.max(a,b);
+// Math.min(a,b);
+//
+// Math.random();
+
+
+//Example 2
+// var a = 15;
+// var b = Math.pow(a,2)+Math.pow(a,3);
+// alert(b);
+
+
+//Example 3
+var a = 5;
+var b = 10;
+
+var randNum = Math.random()*(b-a)+a;
+var intRandNum = Math.round(randNum);
+alert(intRandNum);
